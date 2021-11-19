5b1c8b8e808674fd2851f9fb7ed2b547c27eb2ae
carlos <carlostovar1945@gmail.com>
2021-11-18 23:38:12 -0500

    se agrega el proyecto

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Jose Tovar </span>
+            from <span className="purple"> Venezuela.</span>
+            <br />I am a junior developer aspiring to grow professionally web
+            developer
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/About/Techstack.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMysql,
+  DiPython,
+  DiGit,
+} from "react-icons/di";
+import { SiPytorch, SiTensorflow } from "react-icons/si";
+import { FaVuejs } from "react-icons/fa";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaVuejs />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
